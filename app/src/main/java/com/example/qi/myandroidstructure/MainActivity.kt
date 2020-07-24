baab0085499c267f589ecc2f699fb1c8b610493f
sunbaoqi <15951893223@13.com>
2020-07-24 13:05:24 +0800

    启动优化

--- app/src/main/java/com/example/qi/myandroidstructure/MainActivity.kt
+++ app/src/main/java/com/example/qi/myandroidstructure/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.qi.myandroidstructure
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.bumptech.glide.Glide
+import com.example.qi.myandroidstructure.jetpack.JetpackActivity
 import com.example.qi.myandroidstructure.selfEventBus.MySubscribe
 import com.example.qi.myandroidstructure.selfEventBus.MyEventBus
 import com.example.qi.myandroidstructure.model.Friend
@@ -14,7 +16,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Glide.with(this).load(R.drawable.basketball).into(ivBasket)
+        initListener()
+    }
+
+    private fun initListener() {
+        btnJetpackActivity.setOnClickListener {
+            startActivity(Intent(this@MainActivity, JetpackActivity::class.java))
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/example/qi/myandroidstructure/uiInThread/UiInThreadActivity.kt
@@ -0,0 +1,13 @@
+package com.example.qi.myandroidstructure.uiInThread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.qi.myandroidstructure.R
+
+class UiInThreadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ui_in_thread)
+    }
+}
